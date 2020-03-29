af94a896d6234883cc5371b3263efbd1c3b5df82
FaizHamid <hamidmfaiz@gmail.com>
2020-03-29 15:49:06 -0400

    Grid... coded

--- Grid.py
+++ Grid.py
@@ -2,10 +2,17 @@ from tkinter import *
 
 root = Tk()
 
-# Creating a Label widget
-myLabel = Label(root, text="Hello World!")
-# Shoving it onto the screen
-myLabel.pack()
+# Creating a Label widget and Shoving it onto screen
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="My Name is John Doe").grid(row=1, column=3)
+myLabel3 = Label(root, text="                   ").grid(row=1, column=2)
+
+
+# Shoving it onto the screen in separate step to keep the code look clean
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=3)
+# myLabel3.grid(row=1, column=2)
+
 
 root.mainloop()
 
--- Pack.py
+++ Pack.py
@@ -4,6 +4,7 @@ root = Tk()
 
 # Creating a Label widget
 myLabel = Label(root, text="Hello World!")
+
 # Shoving it onto the screen
 myLabel.pack()
 
